70176915dd03e8793fe91dd775b790f9bb1bc5d2
vamsi <vamsithorlikonda99@gmail.com>
2025-02-20 23:44:47 +0530

    [Vamsi] Add:python script for cal_daily_wages

--- emp.py
+++ emp.py
@@ -1,10 +1,14 @@
-
 import random
 
 def check_attendance():
-    attendance = random.randint(0, 1)  # 0 for Absent, 1 for Present
-    if attendance == 1:
-        return "Employee is Present"
-    return "Employee is Absent"
+    return random.randint(0, 1)  # 0 for Absent, 1 for Present
 
-print(check_attendance())
+def calculate_daily_wage(wage_per_hour=20, full_day_hour=8):
+    if check_attendance() == 1:
+        daily_wage = wage_per_hour * full_day_hour
+        return f"Employee is Present. Daily Wage: {daily_wage}"
+    else:
+        return "Employee is Absent. No Wage for Today."
+
+print(calculate_daily_wage())
+
