d940bcc4faac3eb197857be3ab8952ca68f5f652
vamsi <vamsithorlikonda99@gmail.com>
2025-02-20 21:57:45 +0530

    [Vamsi] Add:python scripted for Attendance

--- emp.py
+++ emp.py
@@ -1,1 +1,10 @@
-print("Hello, Vamsi")
+
+import random
+
+def check_attendance():
+    attendance = random.randint(0, 1)  # 0 for Absent, 1 for Present
+    if attendance == 1:
+        return "Employee is Present"
+    return "Employee is Absent"
+
+print(check_attendance())
