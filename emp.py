c2f4dd9e453346a5a90bafb922dcea82e0d4c533
vamsi <vamsithorlikonda99@gmail.com>
2025-02-21 11:06:01 +0530

    [Vamsi] Add:python_script with switch case

--- emp.py
+++ emp.py
@@ -1,11 +1,22 @@
 import random
 
 def get_work_hours():
-    return random.choice([4, 8])
+    work_type = random.choice(["full_time", "part_time", "absent"])
+    
+    switch_case = {
+        "full_time": 8,
+        "part_time": 4,
+        "absent": 0
+    }
+    
+    return switch_case.get(work_type, 0), work_type
 
 def calculate_daily_wage(wage_per_hour=20):
-    work_hours = get_work_hours()
+    work_hours, work_type = get_work_hours()
     daily_wage = wage_per_hour * work_hours
-    return f"Worked {work_hours} hours. Daily Wage: {daily_wage}"
+
+    if work_type == "absent":
+        return "Employee is Absent. No Wage for Today."
+    return f"Employee worked {work_hours} hours ({work_type}). Daily Wage: {daily_wage}"
 
 print(calculate_daily_wage())
