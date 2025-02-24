6c75ade0d346d5d4015642343f4f42e423a6444b
vamsi <vamsithorlikonda99@gmail.com>
2025-02-24 11:48:38 +0530

    [Vamsi] Add: refactored code with class for multiple companies

--- emp.py
+++ emp.py
@@ -1,33 +1,28 @@
 import random
 
 class EmployeeWage:
-    wage_per_hour = 20
-    full_day_hours = 8
-    part_time_hours = 4
-    working_days_per_month = 20
-    max_hours_per_month = 100
-
-    @classmethod
-    def compute_employee_wage(cls):
+    @staticmethod
+    def compute_employee_wage(company_name, wage_per_hour, max_working_days, max_working_hours):
         total_wage = 0
         total_hours = 0
         total_days = 0
 
-        while total_hours < cls.max_hours_per_month and total_days < cls.working_days_per_month:
+        while total_hours < max_working_hours and total_days < max_working_days:
             total_days += 1
             emp_type = random.choice(["full_time", "part_time", "absent"])
 
             if emp_type == "full_time":
-                daily_hours = cls.full_day_hours
+                daily_hours = 8
             elif emp_type == "part_time":
-                daily_hours = cls.part_time_hours
+                daily_hours = 4
             else:
                 daily_hours = 0
 
             total_hours += daily_hours
-            total_wage += daily_hours * cls.wage_per_hour
+            total_wage += daily_hours * wage_per_hour
 
-        return total_wage
+        print(f"Total Employee Wage for {company_name}: {total_wage}")
 
-print("Total Employee Wage:", EmployeeWage.compute_employee_wage())
+EmployeeWage.compute_employee_wage("Company A", 25, 20, 100)
+EmployeeWage.compute_employee_wage("Company B", 30, 22, 120)
 
